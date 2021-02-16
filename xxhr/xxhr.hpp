4af59fe41efe13ac5a6c24fda1f42fc98d3065d2
Lambourl <65184664+Lambourl@users.noreply.github.com>
2021-02-16 12:57:28 +0100

    :wink: add undef X509_NAME

--- xxhr/xxhr.hpp
+++ xxhr/xxhr.hpp
@@ -3,6 +3,14 @@
 
 #define XXHR_VERSION "v0.0.1"
 
+#if defined(_WIN32) and defined(__MINGW32__)
+# undef X509_NAME
+ # undef X509_CERT_PAIR
+ # undef X509_EXTENSIONS
+ #undef OCSP_REQUEST
+ #undef OCSP_RESPONSE
+#endif
+
 #include "api.hpp"
 #include "auth.hpp"
 #include "xxhrtypes.hpp"
